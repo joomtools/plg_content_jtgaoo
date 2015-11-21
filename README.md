c7ea32ca7883f19a4330a7c3978ee01c251ee8e8
Guido De Gobbis <guido.de.gobbis@joomtools.de>
2015-11-21 02:42:06 +0100

    New Version 1.0.1 Backward compatibility to Joomla! 2.5 Code clearing Regex bugfix

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # plg_content_jtgaoo
-By calling {jtgaoo} it will be insert at a given point in the imprint / privacy-statement the stored information text including Google Analytics opt-out link.
-Also it will activate Google Analytics javascript in the head.
+Enables Google Analytics functionality and adds an opt-out link to disable it for German law, with setting an cookie.
--- jtgaoo.php
+++ jtgaoo.php
@@ -1,10 +1,16 @@
 <?php
 /**
- * @Copyright  JoomTools.de
- * @package    JT - Googla Analytics Opt-Out Plugin for Joomla! 3.4.5 and higher
- * @author     Guido De Gobbis
- * @link       http://www.joomtools.de
- * @license    GNU/GPL <http://www.gnu.org/licenses/>
+ * Plugin for Joomla! 2.5 and higher
+ *
+ * Enables Google Analytics functionality and adds an opt-out
+ * link to disable it for German law, with setting an cookie.
+ *
+ * @package    Joomla.Plugin
+ * @subpackage Content.jtgaoo
+ * @author     Guido De Gobbis <guido.de.gobbis@joomtools.de>
+ * @copyright  2015 JoomTools
+ * @license    GNU/GPLv3 <http://www.gnu.org/licenses/gpl-3.0.de.html>
+ * @link       http://joomtools.de
  */
 
 // no direct access
@@ -17,40 +23,47 @@ class plgContentJtGaoo extends JPlugin
 {
     protected $jsSet = false;
 
-    public function __construct(&$subject, $config)
-    {
-        parent::__construct($subject, $config);
-        $this->loadLanguage('plg_content_jtgaoo');
-    }
-
     public function onContentPrepare($context, &$article, &$params, $limitstart)
     {
         $app = JFactory::getApplication();
+
         if (!$app->isSite())
         {
             return;
         }
 
-        /* Prüfen ob Plugin-Platzhalter im Text ist */
-        if (JString::strpos($article->text, '{jtgaoo}') === false)
+        if (strpos($article->text, '{jtgaoo}') === false)
         {
             return;
         }
 
-        $pluginParams = $this->params->toArray();
-        $regex        = '#(<(\w*+)[^>]*>|){jtgaoo}(</\\2+>|)#siU';
-        $p1           = preg_match($regex, $article->text, $matches);
-        $gaoolink     = '<a class="jtgaoo" rel="nofollow" href="javascript:gaOptout();">'
-            . $this->params->get('jtgaoo_linktext')
-            . '</a>'
-            . '<noscript><br /><div class="alert alert-error">'
-            . JText::_('PLG_CONTENT_JTGAOO_LINK_NOSCRIPT')
-            . '</div></noscript>';
+        $this->loadLanguage('plg_content_jtgaoo');
 
-        $content = $this->params->get('jtgaoo_disclaimer') . $gaoolink;
+        $regex = '@(<(\w*+)[^>]*>|){jtgaoo}(</\2>|)@siU';
+        $p1    = preg_match($regex, $article->text, $matches);
 
         if ($p1)
         {
+            $closeTag = ($matches[2] != '') ? strpos($matches[4], $matches[2]) : true;
+
+            if (!$closeTag)
+            {
+                $matches[0] = str_replace($matches[1], '', $matches[0]);
+            }
+
+            ob_start(); ?>
+            <a class="jtgaoo" rel="nofollow" href="javascript:gaOptout();">
+                <?php echo $this->params->get('jtgaoo_linktext'); ?>
+            </a>
+            <noscript>
+                <span class="alert alert-error" style="display: inline-block;">
+                    <?php echo JText::_('PLG_CONTENT_JTGAOO_LINK_NOSCRIPT'); ?>
+                </span>
+            </noscript>
+            <?php
+            $gaoolink = ob_get_clean();
+            $content  = $this->params->get('jtgaoo_disclaimer') . $gaoolink;
+
             $article->text = str_replace($matches[0], $content, $article->text);
         }
     }
@@ -64,47 +77,34 @@ class plgContentJtGaoo extends JPlugin
         }
 
         $ga_id    = $this->params->get('jtgaoo_ga_id');
-        $gaScript = "
-            var gaProperty = '" . $ga_id . "';
-            var disableStr = 'ga-disable-' + gaProperty;
-                if (document . cookie . indexOf(disableStr + '=true') > -1)
-                {
-                    window[disableStr] = true; }
-                function gaOptout()
-                {
-                    if (window[disableStr] === true) {
-                    alert('Google Analytics wurde auf dieser Seite bereits zu einem früheren Zeitpunkt deaktiviert!');
-                } else {
-                    document . cookie = disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/';
-                    window[disableStr] = true;
-                     alert('Google Analytics wird für diese Seite nun deaktiviert!');
-                   }
-               }
-        ";
+        $gaScript = "var gaProperty='" . $ga_id . "';"
+            . "var disableStr='ga-disable-' + gaProperty;"
+            . "if(document.cookie.indexOf(disableStr + '=true') > -1){"
+            . "window[disableStr]=true;}"
+            . "function gaOptout(){"
+            . "if(window[disableStr]===true){"
+            . "alert('" . JText::_('PLG_CONTENT_JTGAOO_IS_ACTIVE') . "');"
+            . "}else{"
+            . "document.cookie=disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/';"
+            . "window[disableStr]=true;"
+            . "alert('" . JText::_('PLG_CONTENT_JTGAOO_SET_ACTIVE') . "');"
+            . "}}";
 
-        $disabledCode = "ga - disable - " . $ga_id;
-        $cookieSet    = $app->input->cookie->get('ga-disable-' . $ga_id, false, 'boolean');
+        $cookieSet = $app->input->cookie->get('ga-disable-' . $ga_id, false, 'boolean');
 
         if ($cookieSet === false)
         {
-            $gaScript .= "
-                (function (b,o,i,l,e,r) {
-                    b . GoogleAnalyticsObject = l;
-                    b[l] || (b[l] = function ()
-                    {
-                        (b[l] . q = b[l] . q || []).push(arguments)
-                    });b[l] . l = +new Date;e = o . createElement(i);r = o . getElementsByTagName(i)[0];e . src = '//www.google-analytics.com/analytics.js';r . parentNode . insertBefore(e, r)
-                }(window,document,'script','ga'));
-                if(ga('create', '" .$ga_id ."', 'auto') !== undefined) {
-                    console.log(ga);
-                    ga('create', '" . $ga_id . "', 'auto');
-                    ga('set', 'anonymizeIp', true);
-                    ga('send', 'pageview');
-                }
-            ";
+            $gaScript .= "(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;"
+                . "b[l]||(b[l]=function(){(b[l].q=b[l].q||[]).push(arguments)"
+                . "});b[l].l=+new Date;e=o.createElement(i);r=o.getElementsByTagName(i)[0];"
+                . "e.src='//www.google-analytics.com/analytics.js';r.parentNode.insertBefore(e,r)"
+                . "}(window,document,'script','ga'));"
+                . "if(ga('create','" . $ga_id . "','auto')!==undefined){"
+                . "ga('create','" . $ga_id . "','auto');"
+                . "ga('set','anonymizeIp',true);"
+                . "ga('send','pageview');"
+                . "}";
         }
-
-        $test = JFactory::getDocument();
 
         JFactory::getDocument()->addScriptDeclaration($gaScript);
         $this->jsSet = true;
